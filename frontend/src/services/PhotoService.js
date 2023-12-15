a1eb9bd46bc674773217f45db716291a1c44d54f
Leonardo Bezerra <leossbezerra@gmail.com>
2023-12-15 12:17:02 -0300

    Update PhotoService.js

--- frontend/src/services/PhotoService.js
+++ frontend/src/services/PhotoService.js
@@ -8,6 +8,7 @@ const publishPhoto = async(data, token) => {
 		const res = await fetch(api + "/photos", config)
 			.then(res => res.json())
 			.catch(err => err)
+			
 		return res
 	} catch (error) {
 		console.log(error)
