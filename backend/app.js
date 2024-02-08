4e975a58b40f4f657056ee30f1f4e8b970d5d838
Leonardo Bezerra <109699627+leobez@users.noreply.github.com>
2024-02-08 11:29:39 -0300

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended: false}))
 
 // solve CORS
 /* app.use(cors({credentials: true, origin: "http://localhost:5173"})) */
-app.use(cors({credentials: true, origin: "https://reactgramleobez.netlify.app"}))
+app.use(cors({credentials: true, origin: "https://instagramclone-leob.netlify.app"}))
 
 // upload directory
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")))
